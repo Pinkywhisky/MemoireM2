188509013c358a9aabf37eb7abf2f786a6550198
Pinkywhisky <jllsla@free.fr>
2023-03-14 00:22:41 +0100

    memoire 14/03/2023  0:22:41,21

--- Push_Git Memoire.bat
+++ Push_Git Memoire.bat
@@ -1,29 +1,50 @@
-echo off
+:: On n'affiche les lignes de commandes
+@echo off
 
+:: On initialise la variable "erreur" à "0"
 set erreur="0"
 
+:: On place un pointeur
 :debut
+:: on nettoie la fenêtre de l'invite de commande
 cls
+:: Si la variable "erreur" est égale à 1, on demande d'entrer 1 ou 2 
 if /I %erreur% == "1" (echo Merci de faire le choix 1 ou 2 !)
+:: On écrit les choix et les actions proposées
 ECHO [1] Faire un git pull
 ECHO [2] Faire un git push
+:: On attend une entrée quis era stockée dans la variable "choix"
+:: On écrira "Resultat :"
 set /p choix=Resultat :
-
+:: Si la variable "choix" est égale à 1 alors va au pointeur nommé "pull"
 if /I "%choix%" == "1" (goto pull)
+:: Si la variable "choix" est égale à 2 alors va au pointeur nommé "push
 if /I "%choix%" == "2" (goto push)
+:: On initialise la variable "erreur" à 1
 set erreur="1"
+:: On va au pointeur nommé "debut"
 goto :debut
 
+:: on place un pointeur nommé "pull"
 :pull
+:: On effectue des lignes de commandes pour recevoir
+:: Les modifications du site Github
 git pull https://github.com/Pinkywhisky/MemoireM2 master
-ECHO git pull fait !
+ECHO Mise a jour faite dans le dossier local !
+:: On attend l'action de l'utilisateur
 pause
-goto debut
+:: On quitte le programme
+exit
 
+:: on place un pointeur nommé "push"
 :push
+:: On effectue des lignes de commandes pour envoyer
+:: Les modifications sur le site Github
 git add .
 git commit -m "memoire %date% %time%"
 git push https://github.com/Pinkywhisky/MemoireM2 master
-ECHO Mise a jour faite !
+ECHO Mise a jour faite sur github !
+:: On attend l'action de l'utilisateur
 pause
+:: On quitte le programme
 exit
