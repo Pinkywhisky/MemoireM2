147e9f8678746e253ea586e513dcdcf9fedc323c
Pinkywhisky <jllsla@free.fr>
2023-03-20 00:32:16 +0100

    memoire 20/03/2023  0:32:16,92

--- Push_Git Memoire.bat
+++ Push_Git Memoire.bat
@@ -38,12 +38,16 @@ exit
 
 :: on place un pointeur nommé "push"
 :push
+set debut="%date% %time%"
 :: On effectue des lignes de commandes pour envoyer
 :: Les modifications sur le site Github
 git add .
 git commit -m "memoire %date% %time%"
 git push https://github.com/Pinkywhisky/MemoireM2 master
+set fin="%date% %time%"
 ECHO Mise a jour faite sur github !
+echo Debut du programme %debut%
+echo Fin du programme %fin%
 :: On attend l'action de l'utilisateur
 pause
 :: On quitte le programme
